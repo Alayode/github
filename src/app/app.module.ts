cb806cb66065910184d1236b8b2d33de60ca7d6a
Chris Samuel <ksamuel.chris@gmail.com>
2019-04-05 14:04:24 -0400

    created t
    modules needed to run the page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { TopReposComponent } from './top-repos/top-repos.component';
-
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [TopReposComponent],
+  declarations: [AppComponent, TopReposComponent],
   imports: [
     CommonModule,
     AppRoutingModule

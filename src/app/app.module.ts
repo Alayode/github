22d7be83a435069d5006a9a2bfd15bf141595274
Chris Samuel <ksamuel.chris@gmail.com>
2019-04-05 15:15:20 -0400

    --amend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
+import {BrowserModule} from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { TopReposComponent } from './top-repos/top-repos.component';
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent, 
     TopReposComponent,
    
 
-    NgbModule.forRoot(),
-
   ],
   imports: [
     // CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
     BrowserModule,
+    NgbModule.forRoot(),
     FormsModule 
   ]
 })

e9ddba6b9dbde1fa1ead73a7f6b8ee8098715c93
Chris Samuel <ksamuel.chris@gmail.com>
2019-04-05 13:55:03 -0400

    added to appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AppRoutingModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
